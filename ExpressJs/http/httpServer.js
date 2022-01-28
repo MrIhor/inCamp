f146cdd18d70635a5d9d46af135c04cc1405555c
MrIhor <molchanigor54@gmail.com>
2022-01-28 16:47:53 +0200

    add httpServer files

---
+++ ExpressJs/http/httpServer.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+function logRequest({ method, url }, res, next) {
+  console.log(`[${new Date().toISOString()}] ${method} ${url}`);
+  next();
+}
+
+app.use(express.json());
+app.use(logRequest);
+
+const tasks = [{ name: 'Get task' }, { name: 'Create task' }];
+
+app.get('/tasks', (req, res) => res.json(tasks));
+
+app.post('/tasks', (req, res) => {
+  const task = req.body;
+  tasks.push(task);
+  res.json(task);
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server work on localhost:${port}`);
+})
